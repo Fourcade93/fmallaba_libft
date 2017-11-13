8da1ba6d1c5d24f6566ef38facfb8f41504a52b4
Furkat MALLABAEV <fmallaba@e1r12p1.unit.ua>
2017-11-13 21:01:07 +0200

    some correct strcat

--- libft/ft_strcat.c
+++ libft/ft_strcat.c
@@ -6,20 +6,14 @@
 /*   By: fmallaba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/28 19:45:31 by fmallaba          #+#    #+#             */
-/*   Updated: 2017/10/28 20:01:41 by fmallaba         ###   ########.fr       */
+/*   Updated: 2017/11/13 20:59:30 by fmallaba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strcat(char *dest, const char *src)
 {
-	char	*ret;
-
-	ret = dest;
-	while (*dest++)
-		;
-	dest--;
-	while (*src)
-		*dest++ = *src++;
-	*dest = '\0';
-	return (ret);
+	ft_strcpy(dest + (ft_strlen(dest)), src);
+	return (dest);
 }
